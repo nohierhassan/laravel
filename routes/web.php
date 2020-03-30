0603cf6732c7b4596241ec8d2d829ebf41065cb6
Nohier <nohierassan2@gmail.com>
2020-03-30 14:17:59 +0200

    Grouping the URLs

--- routes/web.php
+++ routes/web.php
@@ -13,34 +13,37 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::group(['middleware'=>'auth'], function(){
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    
+    // the url of all courses
+    Route::get('/courses', 'CourseController@index')->name('courses.index');
+    
+    // the url for rendering the form for the first time 
+    Route::get('/courses/create', 'CourseController@create')->name('courses.create');
+    
+    // the url to store the Course in the DB
+    Route::post('/courses', 'CourseController@store')->name('courses.store');
+    
+    // the url for rendering the update form for the first time
+    Route::get('/courses/{course}/edit', 'CourseController@edit')->name('courses.edit');
+    
+    // the url for actually updating the resource
+    Route::put('/courses/{course}', 'CourseController@update')->name('courses.update');
+    
+    // the url to store the Course in the DB
+    Route::delete('/courses/{course}', 'CourseController@destroy')->name('courses.destroy');
+    
+    // the  url of only one course
+    Route::get('/courses/{course}', 'CourseController@show')->name('courses.show');
+   
+    Route::get('/home', 'HomeController@index')->name('home');
+
 });
-
-// the url of all courses
-Route::get('/courses', 'CourseController@index')->name('courses.index')->middleware('auth');
-
-// the url for rendering the form for the first time 
-Route::get('/courses/create', 'CourseController@create')->name('courses.create')->middleware('auth');
-
-// the url to store the Course in the DB
-Route::post('/courses', 'CourseController@store')->name('courses.store')->middleware('auth');
-
-// the url for rendering the update form for the first time
-Route::get('/courses/{course}/edit', 'CourseController@edit')->name('courses.edit')->middleware('auth');
-
-// the url for actually updating the resource
-Route::put('/courses/{course}', 'CourseController@update')->name('courses.update')->middleware('auth');
-
-
-
-// the url to store the Course in the DB
-Route::delete('/courses/{course}', 'CourseController@destroy')->name('courses.destroy')->middleware('auth');
-
-// the  url of only one course
-Route::get('/courses/{course}', 'CourseController@show')->name('courses.show')->middleware('auth');
-
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home')->middleware('auth');
+
